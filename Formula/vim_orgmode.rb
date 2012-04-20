c0379a309f67c752781b6e5e0b887468422a4447
Jay Kline <jay.kline.ctr@hpcmo.hpc.mil>
2012-04-20 14:44:09 -0500

    Rename - to _

--- Formula/vim_orgmode.rb
+++ Formula/vim_orgmode.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class vim-orgmode < Formula
+class vim_orgmode < Formula
   homepage 'https://github.com/jceb/vim-orgmode
   head 'https://github.com/jceb/vim-orgmode.git', :tag => 'v0.4.1'
 
